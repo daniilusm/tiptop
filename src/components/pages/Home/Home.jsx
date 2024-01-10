39166bff336bceac2f326f7175a395bd5ab9728a
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2024-01-10 17:51:00 +0300

    add ball

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Canvas } from '@react-three/fiber';
@@ -7,6 +7,34 @@ import s from './Home.module.scss';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 const Home = ({ className }) => {
+  const ballRef = useRef(null);
+
+  useEffect(() => {
+    // document.addEventListener('mousemove', e => {
+    //   console.info(e);
+    //   const { innerWidth, innerHeight } = e.view;
+    //   const { clientX, clientY } = e;
+    //   const progressX = (clientX / (innerWidth / 100)) * 0.05;
+    //   const progressY = (clientY / (innerHeight / 100)) * 0.05;
+    //   console.info(progressX, progressY);
+    //   console.info(ballRef.current.position);
+    //   ballRef.current.position.x = progressX;
+    //   ballRef.current.position.y = progressY;
+    // });
+
+    setInterval(() => {
+      const timer = Date.now() * 0.03;
+
+      if (ballRef.current) {
+        ballRef.current.position.set(
+          Math.cos(timer * 0.1) * 3.2,
+          Math.abs(Math.cos(timer * 0.2)) * 5 + 2,
+          Math.sin(timer * 0.1) * 3.2
+        );
+      }
+    }, 10);
+  }, []);
+
   return (
     <div className={cx(s.root, className)}>
       <Canvas>
@@ -17,22 +45,72 @@ const Home = ({ className }) => {
           position={[1, 5, 7]}
         />
         <PerspectiveCamera position={[0, 0, 0]} />
-        <mesh position={[0, 0, 0]}>
-          <boxGeometry
-            args={[2, 2, 2]}
+        <mesh
+          position={[0, 10, 0]}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
+          <planeGeometry
+            args={[10, 10]}
             attach="geometry"
           />
-          <meshPhongMaterial
-            color="white"
-            // transparent
-            // opacity={0.9}
-            clipShadows
-            flatShading
-          />
+          <meshPhongMaterial color="white" />
         </mesh>
-        <mesh position={[0, 0, 0]}>
-          <sphereGeometry args={[0.5, 26, 26]} />
-          <meshStandardMaterial color="red" />
+        <mesh
+          position={[0, 0, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        >
+          <planeGeometry
+            args={[10, 10]}
+            attach="geometry"
+          />
+          <meshPhongMaterial color="white" />
+        </mesh>
+        <mesh
+          position={[0, 5, 5]}
+          rotation={[0, Math.PI, 0]}
+        >
+          <planeGeometry
+            args={[10, 10]}
+            attach="geometry"
+          />
+          <meshPhongMaterial color="white" />
+        </mesh>
+        <mesh
+          position={[5, 5, 0]}
+          rotation={[0, -Math.PI / 2, 0]}
+        >
+          <planeGeometry
+            args={[10, 10]}
+            attach="geometry"
+          />
+          <meshPhongMaterial color="white" />
+        </mesh>
+        <mesh
+          position={[-5, 5, 0]}
+          rotation={[0, Math.PI / 2, 0]}
+        >
+          <planeGeometry
+            args={[10, 10]}
+            attach="geometry"
+          />
+          <meshPhongMaterial color="white" />
+        </mesh>
+        <mesh
+          position={[0, 5, -5]}
+          rotation={[0, 0, Math.PI / 2]}
+        >
+          <planeGeometry
+            args={[10, 10]}
+            attach="geometry"
+          />
+          <meshPhongMaterial color="white" />
+        </mesh>
+        <mesh
+          ref={ballRef}
+          position={[0, 0, 0]}
+        >
+          <sphereGeometry args={[1.5, 26, 26]} />
+          <meshStandardMaterial color="black" />
         </mesh>
       </Canvas>
     </div>
